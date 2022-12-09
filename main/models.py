5ef1a5e93cd07ddd966259fb07ebb792bd135ad8
William Santos <willsantos.edf@gmail.com>
2022-12-08 23:15:36 -0300

    Primeiro commit de minha lojavirtual.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_ultima_atualizacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    estoque = models.PositiveIntegerField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_ultima_atualizacao = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='imagens-produtos')
+
+    def __str__(self) -> str:
+        return self.nome
+
