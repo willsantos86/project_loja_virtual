446fb01230dce23dbd90cee99199970f2d274be4
William Santos <willsantos.edf@gmail.com>
2022-12-09 08:53:46 -0300

    Criando o Models e o esquema das tabelas de banco de dados.

--- main/models.py
+++ main/models.py
@@ -1,28 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
-from django.db import models
+
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True, db_index=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_ultima_atualizacao = models.DateTimeField(auto_now_add=True)
+    nome = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    #data_criacao = models.DateTimeField(auto_now_add=True)
+    #data_ultima_atualizacao = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('nome', )
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+    
     def __str__(self) -> str:
         return self.nome
 
+
+
 class Produto(models.Model):
-    nome = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    categoria = models.ForeignKey(Categoria, related_name='produtos', null=True, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    imagem = models.ImageField(upload_to='produtos/', blank=True)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     disponivel = models.BooleanField(default=True)
-    estoque = models.PositiveIntegerField()
+    #estoque = models.PositiveIntegerField()
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_ultima_atualizacao = models.DateTimeField(auto_now_add=True)
-    imagem = models.ImageField(upload_to='imagens-produtos')
+    
 
+    class Meta:
+        ordering = ('nome', )
+        index_together = (('id', 'slug'),)
+    
     def __str__(self) -> str:
         return self.nome
+
 
